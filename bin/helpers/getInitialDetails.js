2cdc8708950551c8dcaf2c15d93e2a629ad1bbbe
Amit Ranpariya <amit.ranpariya@testgrid.io>
2024-02-26 10:09:40 +0530

    Debugging

--- bin/helpers/getInitialDetails.js
+++ bin/helpers/getInitialDetails.js
@@ -28,6 +28,10 @@ exports.getInitialDetails = (bsConfig, args, rawArgs) => {
         } catch (e) {
           responseData = {};
         }
+        logger.info(`err - ${JSON.stringify(err)}`);
+        logger.info(`data - ${JSON.stringify(data)}`);
+        logger.info(`resp - ${JSON.stringify(resp)}`);
+        logger.info(`responseData - ${JSON.stringify(responseData)}`);
         if(resp.statusCode != 200) {
           logger.warn(`Warn: Get Initial Details Request failed with status code ${resp.statusCode}`);
           utils.sendUsageReport(bsConfig, args, responseData["error"], Constants.messageTypes.ERROR, 'get_initial_details_failed', null, rawArgs);
