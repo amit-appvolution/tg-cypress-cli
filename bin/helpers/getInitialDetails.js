019267e7d8ccf238a190eef718e8d6a38de21268
Amit Ranpariya <amit.ranpariya@testgrid.io>
2024-02-26 11:15:59 +0530

    Debugging

--- bin/helpers/getInitialDetails.js
+++ bin/helpers/getInitialDetails.js
@@ -32,26 +32,26 @@ exports.getInitialDetails = (bsConfig, args, rawArgs) => {
         logger.info(`data - ${JSON.stringify(data)}`);
         logger.info(`resp - ${JSON.stringify(resp)}`);
         logger.info(`responseData - ${JSON.stringify(responseData)}`);
-        if(responseData['success']){
-            if (!utils.isUndefined(responseData.grr) && responseData.grr.enabled && !utils.isUndefined(responseData.grr.urls)) {
-                config.uploadUrl = responseData.grr.urls.upload_url;
+        if(resp.statusCode != 200) {
+            logger.warn(`Warn: Get Initial Details Request failed with status code ${resp.statusCode}`);
+            utils.sendUsageReport(bsConfig, args, responseData["error"], Constants.messageTypes.ERROR, 'get_initial_details_failed', null, rawArgs);
+            reject({message: responseData["error"], stacktrace: utils.formatRequest(err, resp, body)});
+        } else {
+            if(responseData.success){
+                if (!utils.isUndefined(responseData.grr) && responseData.grr.enabled && !utils.isUndefined(responseData.grr.urls)) {
+                    config.uploadUrl = responseData.grr.urls.upload_url;
+                }
+                resolve(responseData);
+            }else{
+                logger.error(`${responseData.message}`);
+                utils.sendUsageReport(bsConfig, args, responseData.message, Constants.messageTypes.ERROR, 'get_initial_details_failed', null, rawArgs);
+                reject({message: responseData.message, stacktrace: utils.formatRequest(err, resp, body)});
             }
-            resolve(responseData);
-        }else{
-            logger.error(`${responseData['message']}`);
-            utils.sendUsageReport(bsConfig, args, responseData["message"], Constants.messageTypes.ERROR, 'get_initial_details_failed', null, rawArgs);
-            reject({message: responseData['success'], stacktrace: utils.formatRequest(err, resp, body)});
-        }
-        // if(resp.statusCode != 200) {
-        //   logger.warn(`Warn: Get Initial Details Request failed with status code ${resp.statusCode}`);
-        //   utils.sendUsageReport(bsConfig, args, responseData["error"], Constants.messageTypes.ERROR, 'get_initial_details_failed', null, rawArgs);
-        //   resolve({});
-        // } else {
         //   if (!utils.isUndefined(responseData.grr) && responseData.grr.enabled && !utils.isUndefined(responseData.grr.urls)) {
         //     config.uploadUrl = responseData.grr.urls.upload_url;
         //   }
         //   resolve(responseData);
-        // }
+        }
       }
     });
   });
